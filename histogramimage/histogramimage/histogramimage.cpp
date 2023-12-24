f305d1c7bd353f62c3081b8e49401c451975a726
duswntmd <134353659+duswntmd@users.noreply.github.com>
2023-12-24 23:08:19 +0900

    Add files via upload

---
+++ histogramimage/histogramimage/histogramimage.cpp
@@ -0,0 +1,295 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/video.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
+typedef unsigned char uchar;
+
+using namespace std;
+using namespace cv;
+
+int histogram[256], cdfOfHisto[256], histogramEqual[256];
+
+uchar** uc_alloc(int size_x, int size_y)
+{
+    uchar** m;
+    int i;
+
+    if ((m = (uchar**)calloc(size_y, sizeof(uchar*))) == NULL)
+    {
+        printf("uc_alloc error 1\n");
+        exit(0);
+    }
+    for (i = 0; i < size_y; i++)
+        if ((m[i] = (uchar*)calloc(size_x, sizeof(uchar))) == NULL)
+        {
+            printf("uc_alloc error 2\n");
+            exit(0);
+        }
+    return m;
+}
+
+void read_ucmatrix(int size_x, int size_y, uchar** ucmatrix, const char* filename)
+{
+    int i;
+    FILE* f;
+
+    if ((f = fopen(filename, "rb")) == NULL)
+    {
+        printf("%s File open Error!\n", filename);
+        exit(0);
+    }
+    for (i = 0; i < size_y; i++)
+        if (fread(ucmatrix[i], sizeof(uchar), size_x, f) != size_x)
+        {
+            printf("Data Read Error!\n");
+            exit(0);
+        }
+    fclose(f);
+}
+
+void write_ucmatrix(int size_x, int size_y, uchar** ucmatrix, const char* filename)
+{
+    int i;
+    FILE* f;
+
+    if ((f = fopen(filename, "wb")) == NULL)
+    {
+        printf("%s File open Error! 파일을 찾아보세요\n", filename);
+        exit(0);
+    }
+    for (i = 0; i < size_y; i++)
+        if (fwrite(ucmatrix[i], sizeof(uchar), size_x, f) != size_x)
+        {
+            printf("Data Write Error!\n");
+            exit(0);
+        }
+    fclose(f);
+}
+
+void get_hist1(uchar** img, int X_size, int Y_size)
+{
+    int i, j, tmp;
+    double tmp1;
+    int t, tp, range, hrange;
+    CvSize histoSize, cdfSize;
+    IplImage * imgHisto, * cdflmgHisto;
+
+    histoSize.width = 256;
+    histoSize.height = 256;
+
+    cdfSize.width = 256; 
+    cdfSize.height = 256; 
+
+    imgHisto = cvCreateImage(histoSize, 8, 1);
+    cdflmgHisto = cvCreateImage(cdfSize, 8, 1);
+
+    for (i = 0; i < histoSize.height; i++)
+        for (j = 0; j < histoSize.width; j++)
+        {
+            ((uchar*)(imgHisto->imageData + imgHisto->widthStep * i))[j] = 0;
+        }
+    for (i = 0; i < cdfSize.height; i++)
+        for (j = 0; j < cdfSize.width; j++)
+        {
+            ((uchar*)(cdflmgHisto->imageData + cdflmgHisto->widthStep * i))[j] = 0;
+        }
+
+    tp = X_size * Y_size;
+
+    for (i = 0; i < 256; i++)
+        histogram[i] = 0;
+
+    for (i = 0; i < Y_size; i++)
+        for (j = 0; j < X_size; j++)
+            histogram[img[i][j]]++;
+
+    tmp1 = 0;
+    for (i = 0; i < 256; ++i)
+    {
+        tmp1 = tmp1 > histogram[i] ? tmp1 : histogram[i];
+    }
+
+    for (i = 0; i < 256; ++i)
+    {
+        tmp = (int)255 * (histogram[i] / tmp1);
+
+        cvLine(imgHisto, cvPoint(i, 255), cvPoint(i, 255 - tmp), cvScalar(255, 255, 255), 1, 8, 0);
+    }
+
+    cvShowImage("Histo Equal", imgHisto);
+
+    cdfOfHisto[0] = histogram[0];
+    for (i = 0; i < 256; i++)
+    {
+        cdfOfHisto[i] = cdfOfHisto[i - 1] + histogram[i];
+    }
+
+    tmp1 = (double)cdfOfHisto[255];
+    for (i = 0; i < 256; ++i)
+    {
+        tmp = (int)255 * (cdfOfHisto[i] / tmp1);
+        cvLine(cdflmgHisto, cvPoint(i, 255), cvPoint(i, 255 - tmp), cvScalar(255, 255, 255), 1, 8, 0);
+    }
+    cvShowImage("CDF of Histogram", cdflmgHisto);
+
+    range = cdfOfHisto[255] - cdfOfHisto[0];
+    histogramEqual[0] = 0;
+    for (i = 1; i < 256; ++i) {
+        t = (int)ceil(((cdfOfHisto[i] - cdfOfHisto[0]) * 255.0) / range);
+        histogramEqual[i] = (t < 0) ? 0 : (t > 255) ? 255 : t;
+    }
+
+    cvReleaseImage(&imgHisto);
+    cvReleaseImage(&cdflmgHisto);
+    for (i = 0; i < Y_size; ++i)
+        for (j = 0; j < X_size; ++j)
+        {
+            img[i][j] = histogramEqual[img[i][j]];
+        }
+}
+
+void get_hist(uchar** img, int X_size, int Y_size)
+{
+    int i, j, tmp;
+    double tmp1;
+    int t, tp, range, hrange;
+    CvSize histoSize, cdfSize;
+    IplImage* imgHisto, * cdflmgHisto;
+
+    histoSize.width = 256;
+    histoSize.height = 256;
+
+    cdfSize.width = 256; 
+    cdfSize.height = 256; 
+
+    imgHisto = cvCreateImage(histoSize, 8, 1);
+    cdflmgHisto = cvCreateImage(cdfSize, 8, 1);
+
+    for (i = 0; i < histoSize.height; i++)
+        for (j = 0; j < histoSize.width; j++)
+        {
+            ((uchar*)(imgHisto->imageData + imgHisto->widthStep * i))[j] = 0;
+        }
+    for (i = 0; i < cdfSize.height; i++)
+        for (j = 0; j < cdfSize.width; j++)
+        {
+            ((uchar*)(cdflmgHisto->imageData + cdflmgHisto->widthStep * i))[j] = 0;
+        }
+
+    tp = X_size * Y_size;
+
+    for (i = 0; i < 256; i++)
+        histogram[i] = 0;
+
+    for (i = 0; i < Y_size; i++)
+        for (j = 0; j < X_size; j++)
+            histogram[img[i][j]]++;
+
+    tmp1 = 0;
+    for (i = 0; i < 256; ++i)
+    {
+        tmp1 = tmp1 > histogram[i] ? tmp1 : histogram[i];
+    }
+
+    for (i = 0; i < 256; ++i)
+    {
+        tmp = (int)255 * (histogram[i] / tmp1);
+
+        cvLine(imgHisto, cvPoint(i, 255), cvPoint(i, 255 - tmp), cvScalar(255, 255, 255), 1, 8, 0);
+    }
+
+    cvShowImage("Histo Line", imgHisto);
+
+    cdfOfHisto[0] = histogram[0];
+    for (i = 0; i < 256; i++)
+    {
+        cdfOfHisto[i] = cdfOfHisto[i - 1] + histogram[i];
+    }
+
+    tmp1 = (double)cdfOfHisto[255];
+    for (i = 0; i < 256; ++i)
+    {
+        tmp = (int)255 * (cdfOfHisto[i] / tmp1);
+        cvLine(cdflmgHisto, cvPoint(i, 255), cvPoint(i, 255 - tmp), cvScalar(255, 255, 255), 1, 8, 0);
+    }
+    cvShowImage("original CDF of Histogram", cdflmgHisto);
+
+    range = cdfOfHisto[255] - cdfOfHisto[0];
+    histogramEqual[0] = 0;
+    for (i = 1; i < 256; ++i) {
+        t = (int)ceil(((cdfOfHisto[i] - cdfOfHisto[0]) * 255.0) / range);
+        histogramEqual[i] = (t < 0) ? 0 : (t > 255) ? 255 : t;
+    }
+
+    cvReleaseImage(&imgHisto);
+    for (i = 0; i < Y_size; ++i)
+        for (j = 0; j < X_size; ++j)
+        {
+            img[i][j] = histogramEqual[img[i][j]];
+        }
+}
+
+void uc_free(int size_x, int size_y, uchar** ucmatrix)
+{
+    int i;
+    for (i = 0; i < size_y; i++)
+        free(ucmatrix[i]);
+    free(ucmatrix);
+}
+
+int main(int argc, char* argv[])
+{
+    int i, j;
+    IplImage* cvImg;
+    CvSize imgSize;
+    uchar** img;
+
+    if (argc != 4)
+    {
+        printf("Usage: %s imgData x_size y_size\n", argv[0]);
+        exit(0);
+    }
+
+    imgSize.width = atoi(argv[2]);
+    imgSize.height = atoi(argv[3]);
+
+    img = uc_alloc(imgSize.width, imgSize.height);
+
+    read_ucmatrix(imgSize.width, imgSize.height, img, argv[1]);
+
+    cvImg = cvCreateImage(imgSize, 8, 1);
+
+    for (i = 0; i < imgSize.height; i++)
+        for (j = 0; j < imgSize.width; j++)
+        {
+            ((uchar*)(cvImg->imageData + cvImg->widthStep * i))[j] = img[i][j];
+        }
+
+    cvNamedWindow(argv[1], 1);
+    cvShowImage(argv[1], cvImg);
+    get_hist(img, imgSize.width, imgSize.height);
+
+    for (i = 0; i < imgSize.height; i++)
+        for (j = 0; j < imgSize.width; j++)
+        {
+            ((uchar*)(cvImg->imageData + cvImg->widthStep * i))[j] = img[i][j];
+        }
+
+    cvShowImage("Histogram Equalized", cvImg);
+    get_hist1(img, imgSize.width, imgSize.height);
+
+    cvWaitKey(0);
+    cvDestroyWindow(argv[1]);
+    cvReleaseImage(&cvImg);
+    uc_free(imgSize.width, imgSize.height, img);
+
+    return 0;
+}
---
+++ logical/logical/logical.cpp
@@ -0,0 +1,179 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/video.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <stdio.h>
+
+typedef unsigned char uchar;   // #define uchar unsigned char 라고 해도 같은 뜻임.
+
+using namespace std;
+using namespace cv;
+
+uchar** uc_alloc(int size_x, int size_y)  //uchar을 double로 바꾸는 날이 온다.
+{
+    uchar** m;
+    int i;
+
+    if ((m = (uchar**)calloc(size_y, sizeof(uchar*))) == NULL)
+    {
+        printf("d_alloc error 1\7\n");
+        exit(0);
+    }
+    for (i = 0; i < size_y; i++)
+        if ((m[i] = (uchar*)calloc(size_x, sizeof(uchar))) == NULL)
+        {
+            printf("d_alloc error 2\7\n");
+            exit(0);
+        }
+    return m;
+}
+
+void read_ucmatrix(int size_x, int size_y, uchar** ucmatrix, const char* filename)
+{
+    int i;
+    FILE* f;
+
+    if ((f = fopen(filename, "rb")) == NULL)
+    {
+        printf("%s File open Error!\n", filename);
+        exit(0);
+    }
+    for (i = 0; i < size_y; i++)
+        if (fread(ucmatrix[i], sizeof(uchar), size_x, f) != size_x)
+        {
+            printf("Data Read Error!\n");
+            exit(0);
+        }
+    fclose(f);
+}
+
+void write_ucmatrix(int size_x, int size_y, uchar** ucmatrix, const char* filename)
+{
+    int i;
+    FILE* f;
+
+    if ((f = fopen(filename, "wb")) == NULL)
+    {
+        printf("%s File open Error! 파일을 찾아보세요\n", filename);
+        exit(0);
+    }
+    for (i = 0; i < size_y; i++)
+        if (fwrite(ucmatrix[i], sizeof(uchar), size_x, f) != size_x)
+        {
+            printf("Data Write Error!\n");
+            exit(0);
+        }
+    fclose(f);
+}
+
+void uc_free(int size_x, int size_y, uchar** ucmatrix)
+{
+    int i;
+    for (i = 0; i < size_y; i++)
+        free(ucmatrix[i]);
+    free(ucmatrix);
+}
+
+void circle(uchar** Result, int Row, int Col, double diameter)
+{
+    int i, j;
+    double tmp, xsqure, ysqure;
+
+    for (i = 0; i < Row; i++) {
+        for (j = 0; j < Col; j++) {
+            ysqure = (abs(Row / 2.5 - i)) * (abs(Row / 2.5 - i));
+            xsqure = (abs(Col / 2.5 - j)) * (abs(Col / 2.5 - j));
+
+            tmp = sqrt(ysqure + xsqure);
+
+            if (tmp < diameter) Result[i][j] = 255;
+            else Result[i][j] = 0;
+        }
+    }
+}
+
+void maskand(uchar** in1Img, uchar** in2Img, uchar** outImg, int Row, int Col)
+{
+    int i, j;
+
+    for (i = 0; i < Row; i++)
+        for (j = 0; j < Col; j++)
+        {
+            outImg[i][j] = in1Img[i][j] & in2Img[i][j];
+        }
+}
+
+void maskor(uchar** in1Img, uchar** in2Img, uchar** outImg, int Row, int Col)
+{
+    int i, j;
+
+    for (i = 0; i < Row; i++)
+        for (j = 0; j < Col; j++)
+        {
+            outImg[i][j] = in1Img[i][j] | in2Img[i][j];
+        }
+}
+
+void circle(uchar** Result, int Row, int Col, double diameter);
+void maskand(uchar** in1Img, uchar** in2Img, uchar** outImg, int Row, int Col);
+void maskor(uchar** in1Img, uchar** in2Img, uchar** outImg, int Row, int Col);
+
+int main(int argc, char* argv[])
+{
+    int i, j, Row, Col;
+    //반드시 추가
+    uchar** img;
+    uchar** img2;
+    uchar** Result;
+    uchar** circleimg;
+    uchar** maskandimg;
+    uchar** maskorimg;
+
+    if (argc != 5)
+    {
+        printf("Exe imgData x_size y_size \n");
+        exit(0);
+    }
+
+    Col = atoi(argv[2]);
+    Row = atoi(argv[3]);
+
+    //반드시 추가
+    img = uc_alloc(Col, Row);
+    img2 = uc_alloc(Col, Row);
+    circleimg = uc_alloc(Col, Row);
+    maskandimg = uc_alloc(Col, Row);
+    maskorimg = uc_alloc(Col, Row);
+
+    read_ucmatrix(Col, Row, img, argv[1]);
+    read_ucmatrix(Col, Row, img2, argv[4]);
+
+    // 변수 초기화
+    int Block = 8;
+    double diameter = 20; //결과 0하면 완전검정 100하면 검정에 중간 하얀원 200하면 완전하양
+
+    // 반드시 추가
+    circle(circleimg, Row, Col, diameter);
+    maskand(img, circleimg, maskandimg, Row, Col);
+    maskor(img, img2, maskorimg, Row, Col);
+
+    //write_ucmatrix(Col, Row, maskorimg, "cman256.maskor");
+
+    Mat cvImg(Row, Col, CV_8UC(1));
+
+    for (i = 0; i < Row; i++)
+        for (j = 0; j < Col; j++)
+        {
+            cvImg.at<uchar>(i, j) = maskorimg[i][j];
+        }
+
+    namedWindow(argv[1], WINDOW_AUTOSIZE);
+    imshow(argv[1], cvImg);
+
+    waitKey(0);
+    uc_free(Col, Row, img);
+    uc_free(Col, Row, Result);
+    return 0;
+}
